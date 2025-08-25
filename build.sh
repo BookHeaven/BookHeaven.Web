5d56e6f58a9d88e8a888f767ff0e8a694c390648
heasheartfire <gagi1995@gmail.com>
2025-08-26 01:33:43 +0200

    fix(build): correct dotnet installation command syntax

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-﻿#!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+﻿curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh --c 10.0 --install-dir ./dotnet
+./dotnet-install.sh --version 10.0 --install-dir ./dotnet
 ./dotnet/dotnet version
 ./dotnet/dotnet workload install wasm-tools
 npm run tailwindcss:build
