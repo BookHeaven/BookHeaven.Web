df80bb82df5f4b2591cf43bf16af15b594ab11d2
heasheartfire <gagi1995@gmail.com>
2025-08-26 01:45:35 +0200

    chore(build): add shebang to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-﻿curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+﻿#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --version 10.0 --install-dir ./dotnet
 ./dotnet/dotnet version
